9acd951af054161a62545ed7d51f3417ffdd103a
Debojeet Das <debojeetofficial@gmail.com>
2025-03-06 21:34:06 +0530

    init: added task1 and task2

---
+++ task2/simple_container.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SIMPLE_CONTAINER_ROOT=container_root
+
+mkdir -p $SIMPLE_CONTAINER_ROOT
+
+gcc -o container_prog container_prog.c
+
+## Subtask 1: Execute in a new root filesystem
+
+cp container_prog $SIMPLE_CONTAINER_ROOT/
+
+# 1.1: Copy any required libraries to execute container_prog to the new root container filesystem 
+
+
+echo -e "\n\e[1;32mOutput Subtask 2a\e[0m"
+# 1.2: Execute container_prog in the new root filesystem using chroot. You should pass "subtask1" as an argument to container_prog
+
+
+
+echo "__________________________________________"
+echo -e "\n\e[1;32mOutput Subtask 2b\e[0m"
+## Subtask 2: Execute in a new root filesystem with new PID and UTS namespace
+# The pid of container_prog process should be 1
+# You should pass "subtask2" as an argument to container_prog
+
+
+
+echo -e "\nHostname in the host: $(hostname)"
+
+
+## Subtask 3: Execute in a new root filesystem with new PID, UTS and IPC namespace + Resource Control
+# Create a new cgroup and set the max CPU utilization to 50% of the host CPU. (Consider only 1 CPU core)
+
+
+
+echo "__________________________________________"
+echo -e "\n\e[1;32mOutput Subtask 2c\e[0m"
+# Assign pid to the cgroup such that the container_prog runs in the cgroup
+# Run the container_prog in the new root filesystem with new PID, UTS and IPC namespace
+# You should pass "subtask1" as an argument to container_prog
+
+# Remove the cgroup
+
+
+# If mounted dependent libraries, unmount them, else ignore
